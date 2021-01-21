52f0d9b4024e5842a822b29aaa1967aae27338d2
Roman.Kosartsov <roman.kosartsov@x5.ru>
2021-01-21 17:15:37 +0300

    CBV example

--- main.py
+++ main.py
@@ -1,9 +1,14 @@
 import asyncpg
 import typing
-from fastapi import FastAPI, Request
+from fastapi import FastAPI, Request, APIRouter
+from fastapi_utils.cbv import cbv
+
 import models
 
 app = FastAPI()
+router = APIRouter(
+    prefix='/posts',
+)
 
 
 @app.on_event("startup")
@@ -17,18 +22,24 @@ async def close_db_connection():
     await app.pg_conn.close()
 
 
-@app.post("/posts/", tags=['Examples'], response_model=models.Post)
-async def create_post(request: Request, post: models.PostCreate):
-    pg_conn = request.app.pg_conn
-    post_data = await pg_conn.fetchrow(
-        'insert into posts (title, content) values ($1, $2) RETURNING id, title, content',
-        post.title, post.content
-    )
-    return post_data
+@cbv(router)
+class PostsCBV:
+    request: Request
+
+    @router.post("/", tags=['Examples'], response_model=models.Post)
+    async def create_post(self, post: models.PostCreate):
+        pg_conn = self.request.app.pg_conn
+        post_data = await pg_conn.fetchrow(
+            'insert into posts (title, content) values ($1, $2) RETURNING id, title, content',
+            post.title, post.content
+        )
+        return post_data
+
+    @router.get("/", tags=['Examples'], response_model=typing.List[models.Post])
+    async def get_posts(self):
+        pg_conn = self.request.app.pg_conn
+        posts_data = await pg_conn.fetch('select id, title, content from posts')
+        return posts_data
 
 
-@app.get("/posts/", tags=['Examples'], response_model=typing.List[models.Post])
-async def get_posts(request: Request):
-    pg_conn = request.app.pg_conn
-    posts_data = await pg_conn.fetch('select id, title, content from posts')
-    return posts_data
+app.include_router(router)
